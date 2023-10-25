1b8938b113eedb98325efb0e16c2774f136b8b18
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-10-25 10:39:40 +0100

    initial commit, create vite app, installed boostrap and made card component.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CustomCard from "./components/card/CustomCard";
+
+function App() {
+  return (
+    <>
+      <div>
+        <CustomCard />
+      </div>
+    </>
+  );
+}
+
+export default App;
